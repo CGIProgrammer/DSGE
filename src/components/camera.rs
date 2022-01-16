db9bdcb7fb2267e9c8947fe6155c75d562a9be75
Иван Горбунов <ivangor1997@mail.ru>
2022-01-16 15:15:29 +0800

    Перенос Renderer в отдельный файл

--- src/components/camera.rs
+++ src/components/camera.rs
@@ -13,6 +13,50 @@ pub struct CameraUniform
     pub projection_inverted : Mat4,
 }
 
+impl CameraUniform
+{
+    pub fn identity(aspect: f32, fov: f32, znear: f32, zfar: f32) -> Self
+    {
+        let proj = nalgebra::Perspective3::new(aspect, fov * 3.1415926535 / 180.0, znear, zfar).as_matrix().clone();
+        Self {
+            transform : Mat4::identity(),
+            transform_prev : Mat4::identity(),
+            transform_inverted : Mat4::identity(),
+            transform_prev_inverted : Mat4::identity(),
+            projection : proj,
+            projection_inverted : proj.try_inverse().unwrap(),
+        }
+    }
+
+    pub fn set(&mut self, transform: Mat4)
+    {
+        self.transform = transform;
+        self.transform_inverted = transform.try_inverse().unwrap();
+    }
+
+    pub fn save_transform(&mut self)
+    {
+        self.transform_prev = self.transform;
+        self.transform_prev_inverted = self.transform_inverted;
+    }
+}
+
+impl Default for CameraUniform
+{
+    fn default() -> Self
+    {
+        let proj = nalgebra::Perspective3::new(1.0, 80.0 * 3.1415926535 / 180.0, 0.1, 100.0).as_matrix().clone();
+        Self {
+            transform : Mat4::identity(),
+            transform_prev : Mat4::identity(),
+            transform_inverted : Mat4::identity(),
+            transform_prev_inverted : Mat4::identity(),
+            projection : proj,
+            projection_inverted : proj.try_inverse().unwrap(),
+        }
+    }
+}
+
 impl ShaderStructUniform for CameraUniform
 {
     fn glsl_type_name() -> String
