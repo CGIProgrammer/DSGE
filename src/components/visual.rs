6809c30c64f54a1001f255aa90105f2fc5e1bd79
Иван Горбунов <ivangor1997@mail.ru>
2022-05-14 23:30:15 +0800

    Добавление абстрактных визуальных объектов

--- src/components/visual.rs
+++ src/components/visual.rs
@@ -1,12 +1,28 @@
 use super::camera::CameraUniformData;
 use std::sync::Arc;
 
-use vulkano::command_buffer::{AutoCommandBufferBuilder, PrimaryAutoCommandBuffer};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, PrimaryAutoCommandBuffer, SecondaryAutoCommandBuffer};
 use vulkano::render_pass::RenderPass;
 
 /// # Абстрактное визуальное представление
 /// Придаёт внешний вид объекту `GameObject`.
 pub trait AbstractVisual
 {
-    fn draw(&self, _acbb : &mut AutoCommandBufferBuilder<PrimaryAutoCommandBuffer>, camera: &CameraUniformData, render_pass: Arc<RenderPass>, subpass_id: u32) -> Result<(), String>;
+    fn draw(
+        &self,
+        _acbb : &mut AutoCommandBufferBuilder<PrimaryAutoCommandBuffer>,
+        camera: CameraUniformData,
+        render_pass: Arc<RenderPass>,
+        subpass_id: u32,
+        last_mesh_and_material: (i32, i32)
+    ) -> Result<(i32, i32), String>;
+    
+    fn draw_secondary(
+        &self,
+        _acbb : &mut AutoCommandBufferBuilder<SecondaryAutoCommandBuffer>,
+        camera: CameraUniformData,
+        render_pass: Arc<RenderPass>,
+        subpass_id: u32,
+        last_mesh_and_material: (i32, i32)
+    ) -> Result<(i32, i32), String>;
 }
