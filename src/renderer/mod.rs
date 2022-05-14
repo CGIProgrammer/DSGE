2acacfb6d9b7a4bf992ff6d74cefb5fe76cdd830
Иван Горбунов <ivangor1997@mail.ru>
2022-05-14 23:28:01 +0800

    Форматы пикселей из vulkano

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -1,11 +1,11 @@
 pub mod postprocessor;
-use vulkano::device::{
+use vulkano::{device::{
     Device, DeviceExtensions, Features, Queue, DeviceCreateInfo, QueueCreateInfo,
     physical::{
         PhysicalDevice,
         PhysicalDeviceType
     }
-};
+}, swapchain::ColorSpace, render_pass::Subpass};
 use vulkano::swapchain::{self, AcquireError, Swapchain, Surface, PresentMode, SwapchainCreateInfo};
 use vulkano::image::{view::ImageView, SwapchainImage, ImageUsage, ImageLayout, SampleCount};
 use vulkano::render_pass::{RenderPass, RenderPassCreateInfo, SubpassDescription, AttachmentDescription, AttachmentReference};
@@ -14,9 +14,9 @@ use vulkano::instance::Instance;
 use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage, PrimaryAutoCommandBuffer};
 use winit::window::{Window};
 
-use std::sync::{Arc, LockResult};
+use std::{sync::{Arc, LockResult}, marker::PhantomData, ffi::c_void};
 
-use crate::texture::{Texture, TexturePixelFormat, TextureDimensions, TextureRef};
+use crate::texture::{Texture, TexturePixelFormat, TextureDimensions, TextureRef, TexturePixelFormatFeatures};
 use crate::framebuffer::{Framebuffer, FramebufferRef, FramebufferBinder};
 use crate::mesh::{MeshRef, Mesh};
 use crate::shader::ShaderStructUniform;
@@ -65,11 +65,11 @@ impl GBuffer
         println!("Создание нового G-буфера {}x{}", width, height);
         let device = queue.device();
         let formats = [
-            TexturePixelFormat::RGBA8u,
-            TexturePixelFormat::RGBA8u,
-            TexturePixelFormat::RGBA8u,
-            TexturePixelFormat::RGBA16f,
-            TexturePixelFormat::Depth16u
+            TexturePixelFormat::R8G8B8A8_UNORM,
+            TexturePixelFormat::R8G8B8A8_UNORM,
+            TexturePixelFormat::R8G8B8A8_UNORM,
+            TexturePixelFormat::R16G16B16A16_SFLOAT,
+            TexturePixelFormat::D16_UNORM
         ];
 
         let fb = Framebuffer::new(width, height);
@@ -102,11 +102,9 @@ impl GBuffer
 
         let mut attachments = Vec::new();
         for fmt in formats {
-            let img_layout = match fmt {
-                TexturePixelFormat::Depth16u |
-                TexturePixelFormat::Depth24u |
-                TexturePixelFormat::Depth32f => ImageLayout::DepthStencilAttachmentOptimal,
-                _ => ImageLayout::ColorAttachmentOptimal
+            let img_layout = match fmt.is_depth() {
+                true => ImageLayout::DepthStencilAttachmentOptimal,
+                false => ImageLayout::ColorAttachmentOptimal
             };
             let att = AttachmentDescription {
                 format: Some(fmt.vk_format()),
@@ -153,12 +151,13 @@ impl GBuffer
 pub struct Renderer
 {
     _context : Arc<Instance>,
-    _vk_surface : Arc<Surface<Window>>,
+    _vk_surface : Option<Arc<Surface<Window>>>,
     _device : Arc<Device>,
     _queue : Arc<Queue>,
     _swapchain : Arc<Swapchain<Window>>,
     _sc_images : Vec<Arc<SwapchainImage<Window>>>,
     _sc_textures : Vec<TextureRef>,
+    _last_sc_texture_id : u32,
 
     _frame_finish_event : Option<Box<dyn GpuFuture + 'static>>,
     _need_to_update_sc : bool,
@@ -195,7 +194,6 @@ impl Renderer
         println!("{:?}", dimensions);
         let device_extensions = DeviceExtensions {
             khr_swapchain: true,
-            //ext_extended_dynamic_state: true,
             ..DeviceExtensions::none()
         };
         let (physical_device, queue_family) = PhysicalDevice::enumerate(&vk_instance)
@@ -221,6 +219,7 @@ impl Renderer
             .unwrap();
         let features = Features {
             sampler_anisotropy: true,
+            //push_descriptor: true,
             //extended_dynamic_state: true,
             .. Features::none()
         };
@@ -245,16 +244,28 @@ impl Renderer
             let caps = physical_device.surface_capabilities(&win, Default::default()).unwrap();
             let composite_alpha = caps.supported_composite_alpha.iter().next().unwrap();
     
-            let format = Some(
-                physical_device
-                    .surface_formats(&win, Default::default())
-                    .unwrap()[0]
-                    .0,
-            );
+            let formats: Vec<(vulkano::format::Format, ColorSpace)> =
+                physical_device.surface_formats(&win, Default::default()).unwrap()
+                .into_iter()
+                .filter_map(|a| {
+                    println!("{:?}: {:?}", a.0, a.1);
+                    match a {
+                        (
+                            vulkano::format::Format::R8G8B8_SRGB   |
+                            vulkano::format::Format::R8G8B8A8_SRGB |
+                            vulkano::format::Format::B8G8R8_SRGB   |
+                            vulkano::format::Format::B8G8R8A8_SRGB |
+                            vulkano::format::Format::A8B8G8R8_SRGB_PACK32,
+                            ColorSpace::SrgbNonLinear
+                        ) => Some(a),
+                        _ => None
+                    }
+                })
+                .collect::<_>();
             
             Swapchain::new(device.clone(), win.clone(), SwapchainCreateInfo{
                 min_image_count: caps.min_image_count,
-                image_format: format,
+                image_format: Some(formats[0].0),
                 image_extent: win.window().inner_size().into(),
                 image_usage: ImageUsage::color_attachment(),
                 composite_alpha: composite_alpha,
@@ -276,7 +287,7 @@ impl Renderer
                 ..Default::default()
             },
             AttachmentDescription {
-                format: Some(TexturePixelFormat::Depth16u.vk_format()),
+                format: Some(TexturePixelFormat::D16_UNORM.vk_format()),
                 samples: SampleCount::Sample1,
                 load_op: vulkano::render_pass::LoadOp::Clear,
                 store_op: vulkano::render_pass::StoreOp::Store,
@@ -296,7 +307,7 @@ impl Renderer
         let final_render_pass = RenderPass::new(device.clone(), final_renderpass).unwrap();
 
         let result = Renderer {
-            _vk_surface : win,
+            _vk_surface : Some(win),
             _swapchain : swapchain,
             _aspect : dimensions[0] as f32 / dimensions[1] as f32,
             _context : vk_instance,
@@ -304,8 +315,9 @@ impl Renderer
             _queue : queue.clone(),
             _postprocess_pass : final_render_pass,
             _sc_images : images,
+            _last_sc_texture_id : 0,
             _framebuffers : Vec::new(),
-            _screen_plane : Mesh::make_screen_plane(device.clone()).unwrap(),
+            _screen_plane : Mesh::make_screen_plane(queue.clone()).unwrap(),
             _gbuffer : GBuffer::new(dimensions[0] as u16, dimensions[1] as u16, queue.clone()),
             _need_to_update_sc : true,
             _frame_finish_event : Some(sync::now(device.clone()).boxed()),
@@ -320,14 +332,15 @@ impl Renderer
 
     fn resize(&mut self, width: u16, height: u16)
     {
-        self._gbuffer = GBuffer::new(width, height, self._queue.clone());
+        self._gbuffer = GBuffer::new(width/2, height/2, self._queue.clone());
         /* Создание узлов и связей графа постобработки */
         /* На данный момент это размытие в движении */
         self._postprocessor.reset();
         //let rh = self._postprocessor.rolling_hills(width, height, self._sc_textures[0].take().pix_fmt().clone()).unwrap();
         //let acc = self._postprocessor.acc_mblur_new(width, height, self._sc_textures[0].take().pix_fmt().clone()).unwrap();  // Создание ноды размытия в движении
-        let acc = self._postprocessor.copy_node(width, height, self._sc_textures[0].take().pix_fmt().clone()).unwrap();  // Создание ноды размытия в движении
-        self._postprocessor.link_stages(acc, 0, 0, format!("swapchain_out"));    // Соединение ноды с выходом.
+        //let acc = self._postprocessor.copy_node(width, height, self._sc_textures[0].take().pix_fmt().clone()).unwrap();  // Создание ноды размытия в движении
+        let ((_, _), (ostage, output)) = self._postprocessor.fidelityfx_super_resolution(width, height);
+        self._postprocessor.link_stages(ostage, output, 0, format!("swapchain_out"));    // Соединение ноды с выходом.
         let mut camera = self._camera.as_ref().unwrap().lock().unwrap();
         camera.camera_mut().unwrap().set_projection(width as f32 / height as f32, 60.0 * 3.1415926535 / 180.0, 0.1, 100.0);
         
@@ -335,12 +348,12 @@ impl Renderer
 
     pub fn width(&self) -> u16
     {
-        self._vk_surface.window().inner_size().width as u16
+        self._vk_surface.as_ref().unwrap().window().inner_size().width as u16
     }
 
     pub fn height(&self) -> u16
     {
-        self._vk_surface.window().inner_size().height as u16
+        self._vk_surface.as_ref().unwrap().window().inner_size().height as u16
     }
 
     pub fn update_timer(&mut self, timer: &UniformTime)
@@ -354,10 +367,14 @@ impl Renderer
     /// Как правило необходимо при изменении размера окна
     pub fn update_swapchain(&mut self)
     {
-        let dimensions: [u32; 2] = self._vk_surface.window().inner_size().into();
+        let dimensions: [u32; 2] = self._vk_surface.as_ref().unwrap().window().inner_size().into();
+        let mut usage = self._swapchain.create_info().image_usage;
+        usage.transfer_source = true;
+        usage.transfer_destination = true;
         let (new_swapchain, new_images) =
             match self._swapchain.recreate(SwapchainCreateInfo{
                     image_extent: dimensions,
+                    image_usage: usage,
                     ..self._swapchain.create_info()
                 }) {
                 Ok(r) => r,
@@ -369,7 +386,7 @@ impl Renderer
             height: dimensions[1] as u32,
             array_layers: 0
         };
-        let db = Texture::new_empty_mutex("depth", dims, TexturePixelFormat::Depth16u, self._device.clone()).unwrap();
+        let db = Texture::new_empty_mutex("depth", dims, TexturePixelFormat::D16_UNORM, self._device.clone()).unwrap();
         
         self._swapchain = new_swapchain;
         //let dimensions = new_images[0].dimensions().width_height();
@@ -378,7 +395,11 @@ impl Renderer
         self._sc_textures.clear();
         for image in new_images
         {
-            let cb = Texture::from_vk_image_view(ImageView::new_default(image.clone()).unwrap(), self._device.clone()).unwrap();
+            let cb = Texture::from_vk_image_view(
+                ImageView::new_default(image.clone()).unwrap(),
+                Some(image.clone()),
+                self._device.clone()
+            ).unwrap();
             cb.take_mut().set_vertical_address(crate::texture::TextureRepeatMode::ClampToEdge);
             cb.take_mut().set_horizontal_address(crate::texture::TextureRepeatMode::ClampToEdge);
             cb.take_mut().update_sampler();
@@ -414,37 +435,108 @@ impl Renderer
     /// Фомирует буфер команд GPU
     pub fn build_geametry_pass(&self) -> PrimaryAutoCommandBuffer
     {
+        //let timer = std::time::SystemTime::now();
         let mut command_buffer_builder = AutoCommandBufferBuilder::primary(
             self._device.clone(),
             self._queue.family(),
             CommandBufferUsage::OneTimeSubmit,
         ).unwrap();
-        let gbuffer_rp = &self._gbuffer._geometry_pass;
+        let gbuffer_rp = self._gbuffer._geometry_pass.clone();
         let gbuffer_fb = &mut *self._gbuffer._frame_buffer.take_mut();
-        command_buffer_builder.bind_framebuffer(gbuffer_fb, gbuffer_rp.clone()).unwrap();
-        //command_buffer_builder.set_cull_mode(vulkano::pipeline::graphics::rasterization::CullMode::Front);
-        
+        command_buffer_builder.bind_framebuffer(
+            gbuffer_fb,
+            gbuffer_rp.clone(),
+            false
+        ).unwrap();
         let camera_data = self._camera.as_ref().unwrap().lock().unwrap().camera().unwrap().uniform_data();
+        let mut last_meshmat_pair = (-1, -1);
         for _obj in &self._draw_list
         {
             let locked = _obj.lock().unwrap();
             let obj = locked.visual().unwrap();
-            obj.draw(&mut command_buffer_builder, &camera_data, gbuffer_rp.clone(), 0).unwrap();
+            last_meshmat_pair = obj.draw(
+                &mut command_buffer_builder,
+                camera_data,
+                gbuffer_rp.clone(), 0,
+                last_meshmat_pair
+            ).unwrap();
         }
+        
+        /*
+        let subpass = Subpass::from(gbuffer_rp.clone(), 0).unwrap();
+        let quarter = self._draw_list.len()/4;
+        let half = self._draw_list.len()/2;
+        let tquart = quarter * 3;
+        let mut tasks = Vec::new();
+        for dl in 0..4 {
+            let draw_list = self._draw_list.clone();
+            let viewport = gbuffer_fb.viewport().clone();
+            let device = self._device.clone();
+            let queue = self._queue.clone();
+            let render_pass = gbuffer_rp.clone();
+            let secondary = std::thread::spawn( move || {
+                let mut last_meshmat_pair = (-1, -1);
+                let queue_family = queue.family();
+                let subpass = Subpass::from(render_pass.clone(), 0).unwrap();
+                let mut secondary = AutoCommandBufferBuilder::secondary_graphics(
+                    device.clone(),
+                    queue_family,
+                    CommandBufferUsage::OneTimeSubmit,
+                    subpass
+                ).unwrap();
+                secondary.set_viewport(0, [viewport.clone()]);
+                let begin = draw_list.len()*dl/4;
+                let end = (draw_list.len()*(dl+1)/4).min(draw_list.len());
 
-        command_buffer_builder.end_render_pass().unwrap();
-        command_buffer_builder.build().unwrap()
+                for _obj in &draw_list[begin..end]
+                {
+                    let locked = _obj.lock().unwrap();
+                    let obj = locked.visual().unwrap();
+                    last_meshmat_pair = obj.draw_secondary(
+                        &mut secondary,
+                        camera_data,
+                        render_pass.clone(), 0,
+                        last_meshmat_pair
+                    ).unwrap();
+                }
+                secondary.build().unwrap()
+            });
+            tasks.push(secondary);
+        }
+        
+        for task in tasks {
+            let secondary_cb = task.join().unwrap();
+            command_buffer_builder.execute_commands(secondary_cb).unwrap();
+        }*/
+        
+        command_buffer_builder
+            .end_render_pass().unwrap();
+        let result = command_buffer_builder.build().unwrap();
+        result
+    }
+
+    pub fn last_sc_image(&self) -> TextureRef
+    {
+        self._sc_textures[self._last_sc_texture_id as usize].clone()
+    }
+
+    pub fn wait(&mut self)
+    {
+        //let future = self._frame_finish_event.as_mut().unwrap().clone();
+        //self._frame_finish_event = Some(future.then_signal_semaphore_and_flush().unwrap());
+        self._frame_finish_event.as_mut().unwrap().flush().unwrap();
+        self._frame_finish_event.as_mut().unwrap().cleanup_finished();
+        self._frame_finish_event = Some(sync::now(self.device().clone()).boxed());
     }
 
     /// Выполняет все сформированные буферы команд
     pub fn end_frame(&mut self)
     {
-        self._frame_finish_event.as_mut().unwrap().cleanup_finished();
         if self._need_to_update_sc {
             self.update_swapchain();
             self._need_to_update_sc = false;
-            return;
         }
+
         let (image_num, suboptimal, acquire_future) =
         match swapchain::acquire_next_image(self._swapchain.clone(), None) {
             Ok(r) => r,
@@ -455,23 +547,28 @@ impl Renderer
             }
             Err(e) => panic!("Failed to acquire next image: {:?}", e),
         };
-        
         if suboptimal {
             self._need_to_update_sc = true;
+            return;
         }
-        
+        self._last_sc_texture_id = image_num as _;
+        // Построение прохода геометрии
+        let gp_command_buffer = self.build_geametry_pass();
+        //let geom_pass_time = timer.elapsed().unwrap().as_secs_f64();
+
+        // Построение прохода постобработки
         // Передача входов в постобработку
-        self._postprocessor.image_to_all(&format!("image"), &self._gbuffer._albedo);
+        self._postprocessor.image_to_all(&format!("image"),   &self._gbuffer._albedo);
         //self._postprocessor.image_to_all(&format!("vectors"), &self._gbuffer._vectors);
 
         // Подключение swapchain-изображения в качестве выхода
-        self._postprocessor.set_output(format!("swapchain_out"), self._sc_textures[image_num].clone());
-        
-        // Построение прохода геометрии
-        let gp_command_buffer = self.build_geametry_pass();
-        // Построение прохода постобработки
-        let pp_command_buffer = self._postprocessor.execute_graph();
-
+        //self._postprocessor.set_output(format!("swapchain_out"), self._sc_textures[image_num].clone());
+        let mut pp_command_buffer = self._postprocessor.execute_graph();
+        let sc_out = self._postprocessor.get_output("swapchain_out".to_string()).unwrap();
+        let sc_target = (&self._sc_textures[image_num]).take();
+        Texture::copy(&*(sc_out.take()), &*sc_target, Some(&mut pp_command_buffer), None);
+        let pp_command_buffer = pp_command_buffer.build().unwrap();
+        //let timer = std::time::SystemTime::now();
         let future = self._frame_finish_event
             .take().unwrap()
             .then_execute(self._queue.clone(), gp_command_buffer).unwrap()
@@ -493,7 +590,11 @@ impl Renderer
                 self._frame_finish_event = Some(sync::now(self._device.clone()).boxed());
             }
         };
-        
+        //let future_time = timer.elapsed().unwrap().as_secs_f64();
+        /*println!(
+            "end_frame => update_swapchain {:.3}ms, geom_pass_time {:.3}ms, postprocess_time {:.3}ms, future_time {:.3}ms",
+            update_swapchain_time*1000.0, geom_pass_time*1000.0, postprocess_time*1000.0, future_time*1000.0
+        );*/
     }
 
     pub fn queue(&self) -> &Arc<Queue>
