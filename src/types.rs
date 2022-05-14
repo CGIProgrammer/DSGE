1d5ed5d10f2745807da1c971a21cb71b01991675
Иван Горбунов <ivangor1997@mail.ru>
2022-05-14 23:31:01 +0800

    Форматы пикселей из vulkano

--- src/types.rs
+++ src/types.rs
@@ -8,21 +8,21 @@ pub trait NalgebraPixelType
     fn pix_fmt(&self) -> TexturePixelFormat;
 }
 
-pub type Vec2 = nalgebra::Vector2<f32>; impl NalgebraPixelType for Vec2 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RG32f }}
-pub type Vec3 = nalgebra::Vector3<f32>; impl NalgebraPixelType for Vec3 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RGB32f }}
-pub type Vec4 = nalgebra::Vector4<f32>; impl NalgebraPixelType for Vec4 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RGBA32f }}
-pub type BVec2 = nalgebra::Vector2<bool>; impl NalgebraPixelType for BVec2 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RG8u }}
-pub type BVec3 = nalgebra::Vector3<bool>; impl NalgebraPixelType for BVec3 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RGB8u }}
-pub type BVec4 = nalgebra::Vector4<bool>; impl NalgebraPixelType for BVec4 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RGBA8u }}
-pub type DVec2 = nalgebra::Vector2<f64>; impl NalgebraPixelType for DVec2 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RG64f }}
-pub type DVec3 = nalgebra::Vector3<f64>; impl NalgebraPixelType for DVec3 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RGB64f }}
-pub type DVec4 = nalgebra::Vector4<f64>; impl NalgebraPixelType for DVec4 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RGBA64f }}
-pub type IVec2 = nalgebra::Vector2<i32>; impl NalgebraPixelType for IVec2 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RG32i }}
-pub type IVec3 = nalgebra::Vector3<i32>; impl NalgebraPixelType for IVec3 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RGB32i }}
-pub type IVec4 = nalgebra::Vector4<i32>; impl NalgebraPixelType for IVec4 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RGBA32i }}
-pub type UVec2 = nalgebra::Vector2<u32>; impl NalgebraPixelType for UVec2 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RG32u }}
-pub type UVec3 = nalgebra::Vector3<u32>; impl NalgebraPixelType for UVec3 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RGB32u }}
-pub type UVec4 = nalgebra::Vector4<u32>; impl NalgebraPixelType for UVec4 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::RGBA32u }}
+pub type Vec2 = nalgebra::Vector2<f32>; impl NalgebraPixelType for Vec2 { fn pix_fmt(&self) -> TexturePixelFormat    { TexturePixelFormat::R32G32_SFLOAT }}
+pub type Vec3 = nalgebra::Vector3<f32>; impl NalgebraPixelType for Vec3 { fn pix_fmt(&self) -> TexturePixelFormat    { TexturePixelFormat::R32G32B32_SFLOAT }}
+pub type Vec4 = nalgebra::Vector4<f32>; impl NalgebraPixelType for Vec4 { fn pix_fmt(&self) -> TexturePixelFormat    { TexturePixelFormat::R32G32B32A32_SFLOAT }}
+pub type BVec2 = nalgebra::Vector2<bool>; impl NalgebraPixelType for BVec2 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::R8G8_UNORM }}
+pub type BVec3 = nalgebra::Vector3<bool>; impl NalgebraPixelType for BVec3 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::R8G8B8_UNORM }}
+pub type BVec4 = nalgebra::Vector4<bool>; impl NalgebraPixelType for BVec4 { fn pix_fmt(&self) -> TexturePixelFormat { TexturePixelFormat::R8G8B8A8_UNORM }}
+pub type DVec2 = nalgebra::Vector2<f64>; impl NalgebraPixelType for DVec2 { fn pix_fmt(&self) -> TexturePixelFormat  { TexturePixelFormat::R64G64_SFLOAT }}
+pub type DVec3 = nalgebra::Vector3<f64>; impl NalgebraPixelType for DVec3 { fn pix_fmt(&self) -> TexturePixelFormat  { TexturePixelFormat::R64G64B64_SFLOAT }}
+pub type DVec4 = nalgebra::Vector4<f64>; impl NalgebraPixelType for DVec4 { fn pix_fmt(&self) -> TexturePixelFormat  { TexturePixelFormat::R64G64B64A64_SFLOAT }}
+pub type IVec2 = nalgebra::Vector2<i32>; impl NalgebraPixelType for IVec2 { fn pix_fmt(&self) -> TexturePixelFormat  { TexturePixelFormat::R8G8_SNORM }}
+pub type IVec3 = nalgebra::Vector3<i32>; impl NalgebraPixelType for IVec3 { fn pix_fmt(&self) -> TexturePixelFormat  { TexturePixelFormat::R8G8B8_SNORM }}
+pub type IVec4 = nalgebra::Vector4<i32>; impl NalgebraPixelType for IVec4 { fn pix_fmt(&self) -> TexturePixelFormat  { TexturePixelFormat::R8G8B8A8_SNORM }}
+pub type UVec2 = nalgebra::Vector2<u32>; impl NalgebraPixelType for UVec2 { fn pix_fmt(&self) -> TexturePixelFormat  { TexturePixelFormat::R8G8_UNORM }}
+pub type UVec3 = nalgebra::Vector3<u32>; impl NalgebraPixelType for UVec3 { fn pix_fmt(&self) -> TexturePixelFormat  { TexturePixelFormat::R8G8B8_UNORM }}
+pub type UVec4 = nalgebra::Vector4<u32>; impl NalgebraPixelType for UVec4 { fn pix_fmt(&self) -> TexturePixelFormat  { TexturePixelFormat::R8G8B8A8_UNORM }}
 
 #[allow(dead_code)]
 pub type Mat2 = nalgebra::Matrix2<f32>;
