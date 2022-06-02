9360a9b1bb3fdb96019b69b934f830edd570e5e1
Иван Горбунов <ivangor1997@mail.ru>
2022-06-02 11:15:09 +0800

    Перенос компонентов в их собственные модули

--- src/components/light.rs
+++ src/components/light.rs
@@ -1,36 +1,172 @@
+use std::sync::Arc;
+
+use vulkano::device::Device;
+
+use crate::framebuffer::Framebuffer;
 use crate::types::{Vec3};
-use crate::texture::TextureRef;
+use crate::texture::{Texture, TextureDimensions, TexturePixelFormat};
+use crate::game_object::GameObject;
+use crate::components::ProjectionUniformData;
 
+#[derive(Clone)]
+struct ShadowBuffer(Texture);
 
-#[allow(dead_code)]
-pub struct SunLight {
-    dynamic_shadow_maps: Option<TextureRef>,
-    static_shadow_map: Option<TextureRef>,
-    power: f32,
-    color: Vec3
+impl ShadowBuffer
+{
+    fn new(device: Arc<Device>, resolution: u16, layers: u16) -> Self
+    {
+        let buffer = Texture::new_empty(
+            format!("Shadow buffer {}", resolution).as_str(),
+            TextureDimensions::Dim2d{
+                width: resolution as _,
+                height: resolution as _,
+                array_layers: layers as _
+            },
+            TexturePixelFormat::D16_UNORM,
+            device
+        ).unwrap();
+        Self(buffer)
+    }
+
+    fn shadow_map_framebuffers(&self) -> Vec<Framebuffer>
+    {
+        let width = self.0.width() as u16;
+        let height = self.0.height() as u16;
+        (0..self.0.array_layers()).map(
+            |layer| {
+                let mut fb = Framebuffer::new(width, height);
+                let subbuffer = self.0.array_layer_as_texture(layer).unwrap();
+                fb.set_depth_attachment(&subbuffer, 1.0.into());
+                fb
+            }
+        ).collect::<Vec<_>>()
+    }
 }
 
 #[allow(dead_code)]
-pub struct PointLight {
-    dynamic_shadow_map: Option<TextureRef>,
-    static_shadow_map: Option<TextureRef>,
-    power: f32,
-    color: Vec3
-}
-
-#[allow(dead_code)]
+#[derive(Clone)]
 pub struct SpotLight {
-    dynamic_shadow_map: Option<TextureRef>,
-    static_shadow_map: Option<TextureRef>,
+    dynamic_shadow_buffer: Option<ShadowBuffer>,
     angle: f32,
     inner_angle: f32,
     power: f32,
+    distance: f32,
     color: Vec3
 }
 
+impl SpotLight
+{
+    pub fn new(power: f32, color: [f32; 3], angle: f32, inner_angle: f32, distance: f32, resolution: u16, device: Arc<Device>) -> Self
+    {
+        Self {
+            dynamic_shadow_buffer: if resolution > 0 {Some(ShadowBuffer::new(device, resolution, 1))} else {None},
+            power,
+            color: color.into(),
+            angle,
+            inner_angle,
+            distance
+        }
+    }
+
+    pub fn projection_data(&self, owner: &GameObject) -> ProjectionUniformData
+    {
+        let transform = owner.transform();
+        let projection = nalgebra::Perspective3::new(1.0, self.angle, 0.1, self.distance);
+        let projection_matrix = projection.as_matrix().clone();
+        ProjectionUniformData {
+            transform : transform.global_for_render.as_slice().try_into().unwrap(),
+            transform_prev : transform.global_for_render_prev.as_slice().try_into().unwrap(),
+            transform_inverted : transform.global_for_render.try_inverse().unwrap().as_slice().try_into().unwrap(),
+            transform_prev_inverted : transform.global_for_render_prev.try_inverse().unwrap().as_slice().try_into().unwrap(),
+            projection : projection_matrix.as_slice().try_into().unwrap(),
+            projection_inverted : projection_matrix.try_inverse().unwrap().as_slice().try_into().unwrap(),
+        }
+    }
+
+    pub fn render_pass_data(&self, owner: &GameObject) -> Option<(Framebuffer, ProjectionUniformData)>
+    {
+        match self.dynamic_shadow_buffer {
+            Some(ref shadow_buffer) => {
+                Some((shadow_buffer.shadow_map_framebuffers().remove(0), self.projection_data(owner)))
+            },
+            None => None
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Clone)]
+pub struct SunLight {
+    shadow_buffer: Option<ShadowBuffer>,
+    power: f32,
+    color: Vec3
+}
+
+impl SunLight
+{
+    pub fn new(power: f32, color: [f32; 3], resolution: u16, device: Arc<Device>) -> Self
+    {
+        Self {
+            shadow_buffer: if resolution > 0 {Some(ShadowBuffer::new(device, resolution, 1))} else {None},
+            power,
+            color: color.into()
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Clone)]
+pub struct PointLight {
+    shadow_buffer: Option<ShadowBuffer>,
+    power: f32,
+    color: Vec3
+}
+
+impl PointLight
+{
+    pub fn new(power: f32, color: [f32; 3], resolution: u16, device: Arc<Device>) -> Self
+    {
+        Self {
+            shadow_buffer: if resolution > 0 {Some(ShadowBuffer::new(device, resolution, 1))} else {None},
+            power,
+            color: color.into()
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Clone)]
 pub enum Light
 {
     Spot(SpotLight),
     Point(PointLight),
     Sun(SunLight)
+}
+
+impl Light
+{
+    pub fn framebuffers(&self, owner: &GameObject) -> Vec<(Framebuffer, ProjectionUniformData)>
+    {
+        match self {
+            Self::Spot(light) => {
+                match light.render_pass_data(owner) {
+                    Some(shadow_buffer) => vec![shadow_buffer],
+                    None => Vec::new()
+                }
+            },
+            _ => {
+                panic!("Не реализовано!")
+            }
+        }
+    }
+
+    pub fn ty(&self) -> &str
+    {
+        match self
+        {
+            Self::Spot(_) => "SpotLight",
+            Self::Point(_) => "PointLight",
+            Self::Sun(_) => "SunLight",
+        }
+    }
 }
--- src/components/visual.rs
+++ src/components/visual.rs
@@ -1,8 +1,12 @@
+use std::any::Any;
+
 use crate::components::Component;
+use crate::game_object::GOTransform;
 
 use vulkano::command_buffer::{AutoCommandBufferBuilder, PrimaryAutoCommandBuffer};
+use vulkano::render_pass::Subpass;
 
-use super::{GameObject, Renderer};
+use super::{GameObject, Renderer, GOTransformUniform};
 use crate::mesh::{MeshRef};
 use crate::material::{MaterialRef};
 use crate::references::*;
@@ -11,43 +15,62 @@ use crate::shader::ShaderProgramBinder;
 use crate::mesh::MeshBinder;
 
 pub use crate::components::{
-    camera::CameraUniformData
+    ProjectionUniformData,
 };
 
 #[derive(Clone)]
 pub struct MeshVisual
 {
     mesh : MeshRef,
-    material : MaterialRef
+    material : MaterialRef,
+    cast_shadow : bool
 }
 
 impl MeshVisual
 {
-    pub fn new(mesh: MeshRef, material: MaterialRef) -> Self
+    pub fn new(mesh: MeshRef, material: MaterialRef, cast_shadow: bool) -> Self
     {
         Self {
             mesh : mesh,
-            material : material
+            material : material,
+            cast_shadow : cast_shadow
         }
+    }
+
+    pub fn mesh(&self) -> &MeshRef
+    {
+        &self.mesh
+    }
+
+    pub fn material(&self) -> &MaterialRef
+    {
+        &self.material
     }
 }
 
 impl Component for MeshVisual
 {
+    fn as_any(&self) -> &dyn Any
+    {
+        self
+    }
+
+    /*fn on_render_init(&mut self, owner: &GameObject, renderer: &mut Renderer) -> Result<ProjectionUniformData, ()>
+    {
+        //renderer.add_renderable_component(owner.transform().uniform_value(), self);
+        Ok(())
+    }*/
+
     fn on_geometry_pass(
         &mut self,
-        owner: &GameObject,
-        renderer: &mut Renderer,
+        transform: GOTransformUniform,
+        camera_data: ProjectionUniformData,
+        subpass: Subpass,
         acbb: &mut AutoCommandBufferBuilder<PrimaryAutoCommandBuffer>,
-        last_mesh_and_material: (i32, i32)
-    ) -> Result<(i32, i32), String>
+        new_mesh: bool,
+        new_material: bool
+    ) -> Result<(), String>
     {
-        let subpass = renderer.geometry_subpass();
-        let camera = renderer.camera_data();
-        let mesh_id = self.mesh.box_id();
-        let material_id = self.material.box_id();
-        let prev_mesh_id = last_mesh_and_material.0;
-        let prevmaterial_id = last_mesh_and_material.1;
         let mut material = self.material.take_mut();
         let (shader, mut uniform_buffer) = material.base_shader(subpass);
         let mut shader = shader.clone();
@@ -55,22 +78,69 @@ impl Component for MeshVisual
         let _mesh = self.mesh.take();
         let mesh = _mesh.clone();
         drop(_mesh);
-        if material_id != prevmaterial_id {
-            uniform_buffer.uniform(camera, crate::material::SHADER_CAMERA_SET, 0);
+        if new_material {
+            uniform_buffer.uniform(camera_data, crate::material::SHADER_CAMERA_SET, 0);
             acbb
-                .bind_shader_program(&shader)?
+                .bind_shader_program(&shader).unwrap()
                 .bind_shader_uniforms(&mut uniform_buffer, false)?
         } else {
             acbb.bind_shader_uniforms(&mut uniform_buffer, true)?
         };
-        acbb.bind_uniform_constant(&mut shader, owner.transform().uniform_value())?;
-        if mesh_id != prev_mesh_id {
+        acbb.bind_uniform_constant(&mut shader, transform)?;
+        if new_mesh {
             acbb.bind_mesh(&mesh)?;
         }
         else
         {
             acbb.draw_mesh(&mesh)?;
+        };
+        Ok(())
+    }
+
+    fn on_shadowmap_pass(
+        &mut self,
+        transform: GOTransformUniform,
+        camera_data: ProjectionUniformData,
+        subpass: Subpass,
+        acbb: &mut AutoCommandBufferBuilder<PrimaryAutoCommandBuffer>,
+        new_mesh: bool,
+        new_material: bool
+    ) -> Result<(), String>
+    {
+        if !self.cast_shadow {
+            return Ok(());
         }
-        Ok((mesh_id, material_id))
+        let mut material = self.material.take_mut();
+        let (shader, mut uniform_buffer) = material.base_shadowmap_shader(subpass);
+        let mut shader = shader.clone();
+        drop(material);
+        let _mesh = self.mesh.take();
+        let mesh = _mesh.clone();
+        drop(_mesh);
+        if new_material {
+            uniform_buffer.uniform(camera_data, crate::material::SHADER_CAMERA_SET, 0);
+            acbb
+                .bind_shader_program(&shader).unwrap()
+                .bind_shader_uniforms(&mut uniform_buffer, false)?
+        } else {
+            acbb.bind_shader_uniforms(&mut uniform_buffer, true)?
+        };
+        acbb.bind_uniform_constant(&mut shader, transform)?;
+        if new_mesh {
+            acbb.bind_mesh(&mesh)?;
+        }
+        else
+        {
+            acbb.draw_mesh(&mesh)?;
+        };
+        Ok(())
+    }
+
+    fn mesh_id(&self) -> i32 {
+        self.mesh.box_id()
+    }
+
+    fn material_id(&self) -> i32 {
+        self.material.box_id()
     }
 }
