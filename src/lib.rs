729bff8bfd434092b2e548c75e541c0df8b1cc70
Иван Горбунов <ivangor1997@mail.ru>
2022-06-28 02:03:14 +0800

    Разделение базового примера и библиотеки

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+extern crate winit;
+extern crate vulkano;
+extern crate bytemuck;
+
+pub mod teapot;
+pub mod time;
+
+pub mod game_logic;
+#[macro_use]
+pub mod shader;
+pub mod mesh;
+pub mod types;
+pub mod glenums;
+pub mod utils;
+pub mod references;
+pub mod framebuffer;
+pub mod texture;
+pub mod material;
+#[macro_use]
+pub mod game_object;
+pub mod renderer;
+#[macro_use]
+pub mod components;
+pub mod scene_loader;
+//pub mod scene;
